f6b3c0ea3290ae2ff4031ae1e92bd22d6a01bc1a
Zbynek Cervinka <zcervink@redhat.com>
2021-06-09 19:26:08 +0200

    ++

--- src/it/java/org/jboss/tools/intellij/quarkus/BasicTests.java
+++ src/it/java/org/jboss/tools/intellij/quarkus/BasicTests.java
@@ -37,37 +37,6 @@ public class BasicTests {
     private static RemoteRobot robot;
     private static GlobalUtils.IdeaVersion ideaVersion;
 
-    @BeforeAll
-    public static void connect() throws InterruptedException {
-
-        String acceptedSourceLocation = "accepted";
-        String acceptedDir = System.getProperty("user.home") + "\\AppData\\Roaming\\JetBrains\\consentOptions";
-        createDirectoryHierarchy(acceptedDir);
-        copyFileFromJarResourceDir(acceptedSourceLocation, acceptedDir + "\\accepted");
-
-        String registryPath = "HKCU:\\Software\\JavaSoft\\Prefs\\jetbrains\\privacy_policy";
-        ProcessBuilder pb1 = new ProcessBuilder("powershell.exe", "New-Item", "-Path", registryPath, "-Force");
-        ProcessBuilder pb2 = new ProcessBuilder("powershell.exe", "New-ItemProperty", "-Path", registryPath, "-Name", "accepted_version", "-Value", "\"2.1\"");
-
-        try {
-            Process p1 = pb1.start();
-            p1.waitFor();
-            Process p2 = pb2.start();
-            p2.waitFor();
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
-
-
-
-
-
-        GlobalUtils.waitUntilIntelliJStarts(8082);
-        robot = GlobalUtils.getRemoteRobotConnection(8082);
-        GlobalUtils.clearTheWorkspace(robot);
-        ideaVersion = GlobalUtils.getTheIntelliJVersion(robot);
-    }
-
     private static void createDirectoryHierarchy(String location) {
         Path path = Paths.get(location);
         try {
@@ -99,72 +68,27 @@ public class BasicTests {
         }
     }
 
-    @AfterEach
-    public void finishTheTestRun() {
-        GlobalUtils.checkForExceptions(robot);
-        GlobalUtils.clearTheWorkspace(robot);
-    }
 
     @Test
     public void createAQuarkusProjectAndBuildItUsingMaven() {
-        step("Create a Quarkus project and build it using maven", () -> {
-            QuarkusUtils.createNewQuarkusProject(robot, BuildUtils.ToolToBuildTheProject.MAVEN, QuarkusUtils.EndpointURLType.DEFAULT);
-            GlobalUtils.waitUntilTheProjectImportIsComplete(robot);
-            GlobalUtils.closeTheTipOfTheDayDialogIfItAppears(robot);
-            GlobalUtils.maximizeTheIdeWindow(robot);
-            GlobalUtils.waitUntilAllTheBgTasksFinish(robot);
-            BuildUtils.buildTheProject(robot, BuildUtils.ToolToBuildTheProject.MAVEN);
-            GlobalUtils.waitUntilAllTheBgTasksFinish(robot);
-            BuildUtils.testIfBuildIsSuccessful(robot);
-            GlobalUtils.closeTheProject(robot);
-        });
-    }
+        System.out.println("asdsa");
 
-    @Test
-    public void testIfTheQuarkusRuntimeIsDownloaded() {
-        step("Test whether the Quarkus runtime can be downloaded", () -> {
-            String projectName = "java-project-with-quarkus-runtime";
-            String runtimeJarName = QuarkusUtils.createNewJavaProjectWithQuarkusFramework(robot, projectName);
-            GlobalUtils.waitUntilTheProjectImportIsComplete(robot);
-            GlobalUtils.closeTheTipOfTheDayDialogIfItAppears(robot);
-            GlobalUtils.maximizeTheIdeWindow(robot);
-            GlobalUtils.waitUntilAllTheBgTasksFinish(robot);
-            assertTrue(ProjectToolWindowUtils.isAProjectFilePresent(robot, projectName, "lib", runtimeJarName), "The runtime has not been downloaded.");
-            GlobalUtils.closeTheProject(robot);
-        });
-    }
+        String acceptedSourceLocation = "accepted";
+        String acceptedDir = System.getProperty("user.home") + "\\AppData\\Roaming\\JetBrains\\consentOptions";
+        createDirectoryHierarchy(acceptedDir);
+        copyFileFromJarResourceDir(acceptedSourceLocation, acceptedDir + "\\accepted");
 
-    @Test
-    public void createNewQuarkusProjectWithValidCustomEndpointURL() {
-        step("Create new Quarkus project with valid custom endpoint URL", () -> {
-            QuarkusUtils.createNewQuarkusProject(robot, BuildUtils.ToolToBuildTheProject.MAVEN, QuarkusUtils.EndpointURLType.CUSTOM);
-            GlobalUtils.waitUntilTheProjectImportIsComplete(robot);
-            GlobalUtils.closeTheTipOfTheDayDialogIfItAppears(robot);
-            GlobalUtils.maximizeTheIdeWindow(robot);
-            GlobalUtils.waitUntilAllTheBgTasksFinish(robot);
-            GlobalUtils.closeTheProject(robot);
-        });
-    }
+        String registryPath = "HKCU:\\Software\\JavaSoft\\Prefs\\jetbrains\\privacy_policy";
+        ProcessBuilder pb1 = new ProcessBuilder("powershell.exe", "New-Item", "-Path", registryPath, "-Force");
+        ProcessBuilder pb2 = new ProcessBuilder("powershell.exe", "New-ItemProperty", "-Path", registryPath, "-Name", "accepted_version", "-Value", "\"2.1\"");
 
-    @Test
-    public void createNewQuarkusProjectWithInvalidCustomEndpointURL() {
-        step("Create new Quarkus project with invalid custom endpoint URL", () -> {
-            QuarkusUtils.tryToCreateNewQuarkusProjectWithInvalidCustomEndpointURL(robot);
-        });
-    }
-
-    @Test
-    public void createAQuarkusProjectAndBuildItUsingGradle() {
-        step("Create a Quarkus project and build it using gradle", () -> {
-            QuarkusUtils.createNewQuarkusProject(robot, BuildUtils.ToolToBuildTheProject.GRADLE, QuarkusUtils.EndpointURLType.DEFAULT);
-            GlobalUtils.waitUntilTheProjectImportIsComplete(robot);
-            GlobalUtils.closeTheTipOfTheDayDialogIfItAppears(robot);
-            GlobalUtils.maximizeTheIdeWindow(robot);
-            GlobalUtils.waitUntilAllTheBgTasksFinish(robot);
-            BuildUtils.buildTheProject(robot, BuildUtils.ToolToBuildTheProject.GRADLE);
-            GlobalUtils.waitUntilAllTheBgTasksFinish(robot);
-            BuildUtils.testIfBuildIsSuccessful(robot);
-            GlobalUtils.closeTheProject(robot);
-        });
+        try {
+            Process p1 = pb1.start();
+            p1.waitFor();
+            Process p2 = pb2.start();
+            p2.waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
